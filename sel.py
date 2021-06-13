7e9c44c769f90ac6008cf82a05945461034beceb
GiovaGa <giovanni.gaio02@gmail.com>
2021-06-13 19:07:22 +0200

    Added some comments

--- sel.py
+++ sel.py
@@ -1,20 +1,26 @@
 from time import sleep
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-cod_fisc = ""
-num_tes = ""
+cod_fisc = "" # insert your codice fiscale
+num_tes = ""    # insert the number behind your tessera sanitaria 
 
 url = "https://cup.apss.tn.it/webportal/vaccinocovid/welcome"
 
 driver = webdriver.Firefox()
 driver.implicitly_wait(5)
+
+
+#### Wait the right time ####
+
+
 driver.get(url)
 
-### go to booking page ###
+######## go to booking page ########
 driver.find_element_by_css_selector("div.widget:nth-child(1)").click()
 
 ########## insert info ##########
@@ -33,7 +39,7 @@ except:
     ### Let's go the hard way
     from pynput.keyboard import Key, Controller
     print("Going the hard way")
-    sleep(0.3)
+    sleep(0.2) # may depend on the connection speed
     keyboard = Controller()
 
     keyboard.press(Key.tab)
@@ -44,8 +50,8 @@ except:
     keyboard.type(num_tes)
     
     pass
-# check the checkboxes
 
+#### check the checkboxes ####
 i = 1
 while True:
     cssel = "form.ng-dirty > div:nth-child(2) > div:nth-child(4) > div:nth-child(" + str(i) + ") > label:nth-child(1) > i:nth-child(1)"
@@ -55,7 +61,8 @@ while True:
     except: break
     i += 1
 
-sleep(0.1)
+#### Hit the continue button ####
+# sleep(0.1)
 css_continue = ".wizard-body > team-card-login:nth-child(1) > div:nth-child(1) > ng-include:nth-child(2) > form:nth-child(1) > div:nth-child(3) > button:nth-child(2)"
 driver.find_element_by_css_selector(css_continue).click()
 
